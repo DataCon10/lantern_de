78fa771a93317c19f59f4c329fe2964f8d877c59
Kelly, Conor <conor.kelly@astrazeneca.com>
2025-04-09 15:43:07 +0100

    Resolved for potential unwanted tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="myapp",
     version="0.1",
-    packages=find_packages(),  # Automatically finds the myapp package.
+    packages=find_packages(exclude=["myapp.tests", "myapp.tests.*"]),  # Automatically finds the myapp package.
     install_requires=[
         "requests",
     ],
