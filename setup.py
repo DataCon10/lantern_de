64229755d19f78855a523c50ea54be5de45b3d28
Kelly, Conor <conor.kelly@astrazeneca.com>
2025-04-09 11:38:45 +0100

    Debugging CI setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="myapp",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["myapp.tests", "myapp.tests.*"]),
     install_requires=[
         "requests",
     ],
