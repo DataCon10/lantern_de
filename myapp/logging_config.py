cf439d50e2ccc452e09b005e1fbcd366f8bf2e67
Kelly, Conor <conor.kelly@astrazeneca.com>
2025-04-10 09:01:18 +0100

    Logging centralisation

---
+++ myapp/logging_config.py
@@ -0,0 +1,29 @@
+# myapp/logging_config.py
+
+import os
+import logging
+from logging.config import dictConfig
+
+def configure_logging():
+    log_level = os.environ.get("LOG_LEVEL", "INFO")
+    LOGGING_CONFIG = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+                "level": log_level,
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": log_level,
+        },
+    }
+    dictConfig(LOGGING_CONFIG)
